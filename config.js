7a29a034030beb8d05e03f3f65b7ac01759958cc
Jason <github@thingymail.com>
2024-04-03 23:52:59 -0400

    Use Github Secrets in place of variables

--- config.js
+++ config.js
@@ -1,24 +1,25 @@
-const projection_Labs_api_key = "YOUR_PL_API_KEY";
+const projection_Labs_api_key = process.env.PROJECTION_LABS_API_KEY;
 
 const monarchCredentials = {
-    monarch_email: "MONARCH_EMAIL_GOES_HERE",
-    monarch_password: "MONARCH_PASSWORD_GOES_HERE",
-    monarch_mfa: null
+    monarch_email: process.env.MONARCH_EMAIL,
+    monarch_password: process.env.MONARCH_PASSWORD,
+    monarch_mfa: process.env.MONARCH_MFA
 };
 
-// Create an object array to store ProjectionLab account, monarchAccountID,and balance
-// Create a record for each account you want to update
-let accountMapping = [
-    {
-        plAccountID: "PROJECTION_LABS_ACCOUNT_ID_GOES_HERE",
-        monarchAccountID: "MONARCH_ACCOUNT_ID_GOES_HERE",
-        balance: null
-    },
-    {
-        plAccountID: "PROJECTION_LABS_ACCOUNT_ID_GOES_HERE",
-        monarchAccountID: "MONARCH_ACCOUNT_ID_GOES_HERE",
-        balance: null
-    }
-];
+// Comment out after getting account mapping locally
+// let accountMapping = [
+//     {
+//         plAccountID: "PROJECTION_LABS_ACCOUNT_ID_GOES_HERE",
+//         monarchAccountID: "MONARCH_ACCOUNT_ID_GOES_HERE",
+//         balance: null
+//     },
+//     {
+//         plAccountID: "PROJECTION_LABS_ACCOUNT_ID_GOES_HERE",
+//         monarchAccountID: "MONARCH_ACCOUNT_ID_GOES_HERE",
+//         balance: null
+//     }
+// ];
+
+let accountMapping = JSON.parse(process.env.ACCOUNT_MAPPING);
 
 export { projection_Labs_api_key, monarchCredentials, accountMapping };
